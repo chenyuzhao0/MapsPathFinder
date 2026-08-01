d3d5c2c17f01cd7d277878c868dd5a01416cf505
Chen Yu <chenyuzhao053@gmail.com>
2026-08-01 17:36:20 +0200

    feat: draft MapGraph doors

---
+++ src/MapGraph.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class MapGraph {
+    private HashMap<Integer,Intersection> graph;
+
+    public MapGraph(){
+        this.graph = new HashMap<>();
+    }
+
+    public Intersection addIntersection(int id){
+
+    }
+    public Road connectRoad(Intersection start, Intersection end, int seconds, int distanceInMeters){
+
+    }
+}
